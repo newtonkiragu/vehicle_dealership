64efb8f98a89e0ec2bb826296a76fd6bb506cb52
newtonkiragu <karanunewton4@gmail.com>
2017-07-05 16:53:35 +0300

    fixed all the specs and the code is wroking perfectly

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,30 +1,15 @@
 class Vehicle
   @@vehicles = []
+  attr_reader(:make, :model, :year, :color, :engine_size, :number_of_doors, :id)
 
-  define_method(:initialize) do |make, model, year, color, engine_size, number_of_doors|
-    @make = make
-    @model = model
-    @year = year
-    @color = color
-    @engine_size = engine_size
-    @number_of_doors = number_of_doors
+  define_method(:initialize) do |attributes|
+    @make = attributes.fetch(:make)
+    @model = attributes.fetch(:model)
+    @year = attributes.fetch(:year)
+    @color = attributes.fetch(:color)
+    @engine_size = attributes.fetch(:engine_size)
+    @number_of_doors = attributes.fetch(:number_of_doors)
     @id = @@vehicles.length().+(1)
-  end
-
-  define_method(:make) do
-    @make
-  end
-
-  define_method(:model) do
-    @model
-  end
-
-  define_method(:year) do
-    @year
-  end
-
-  define_method(:id) do
-    @id
   end
 
   define_singleton_method(:all) do
@@ -39,16 +24,6 @@ class Vehicle
     @@vehicles = []
   end
 
-  define_method(:age) do
-    current_year = Time.new().year()
-    current_year.-(@year)
-  end
-
-  define_method(:worth_buying?) do
-    american_cars = ["Chrysler", "Ford", "GM"]
-    american_cars.include?(@make).&(self.age().<=(15))
-  end
-
   define_singleton_method(:find) do |identification|
     found_vehicle = nil
     @@vehicles.each() do |vehicle|
@@ -58,4 +33,10 @@ class Vehicle
     end
     found_vehicle
   end
+  
+  define_method(:age) do
+    current_year = Time.new().year()
+    current_year.-(@year).to_i
+  end
+
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -8,21 +8,21 @@ describe('Vehicle') do
 
   describe("#make") do
     it("returns the make of the vehicle") do
-      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_vehicle = Vehicle.new({:make=> "Toyota", :model => "Prius", :year => "2000", :color => "blue", :engine_size => "4L", :number_of_doors => "4"})
       expect(test_vehicle.make()).to(eq("Toyota"))
     end
   end
 
   describe("#model") do
     it("returns the model of the vehicle") do
-      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_vehicle = Vehicle.new({:make=> "Toyota", :model => "Prius", :year => "2000", :color => "blue", :engine_size => "4L", :number_of_doors => "4"})
       expect(test_vehicle.model()).to(eq("Prius"))
     end
   end
 
   describe("#year") do
     it("returns the year of the vehicle") do
-      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_vehicle = Vehicle.new({:make=> "Toyota", :model => "Prius", :year => 2000, :color => "blue", :engine_size => "4L", :number_of_doors => "4"})
       expect(test_vehicle.year()).to(eq(2000))
     end
   end
@@ -35,7 +35,7 @@ describe('Vehicle') do
 
   describe("#save") do
     it("adds a vehicle to the array of saved vehicles") do
-      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_vehicle = Vehicle.new({:make=> "Toyota", :model => "Prius", :year => "2000", :color => "blue", :engine_size => "4L", :number_of_doors => "4"})
       test_vehicle.save()
       expect(Vehicle.all()).to(eq([test_vehicle]))
     end
@@ -43,7 +43,7 @@ describe('Vehicle') do
 
   describe(".clear") do
     it("empties out all of the saved vehicles") do
-      Vehicle.new("Toyota", "Prius", 2000).save()
+      Vehicle.new({:make=> "Toyota", :model => "Prius", :year => "2000", :color => "blue", :engine_size => "4L", :number_of_doors => "4"}).save()
       Vehicle.clear()
       expect(Vehicle.all()).to(eq([]))
     end
@@ -51,29 +51,23 @@ describe('Vehicle') do
 
   describe("#age") do
     it("returns the vehicles age") do
-      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_vehicle = Vehicle.new({:make=> "Toyota", :model => "Prius", :year => "2000".to_i, :color => "blue", :engine_size => "4L", :number_of_doors => "4"})
       expect(test_vehicle.age()).to(eq(17))
     end
   end
 
-  describe("#worth_buying?") do
-    it("returns false if the car is not American made and less than 15 years old") do
-      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
-      expect(test_vehicle.worth_buying?()).to(eq(false))
-    end
-  end
   describe("#id")do
     it("returns the id of the vehicle")do
-      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_vehicle = Vehicle.new({:make=> "Toyota", :model => "Prius", :year => 2000, :color => "blue", :engine_size => "4L", :number_of_doors => "4"})
       test_vehicle.save()
       expect(test_vehicle.id()).to(eq(1))
     end
   end
   describe(".find")do
     it("returns a vehicle by its id number")do
-      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_vehicle = Vehicle.new({:make=> "Toyota", :model => "Prius", :year => "2000", :color => "blue", :engine_size => "4L", :number_of_doors => "4"})
       test_vehicle.save()
-      test_vehicle2 = Vehicle.new("Toyota", "Corolla", 1999)
+      test_vehicle2 = Vehicle.new({:make=> "Toyota", :model => "Prius", :year => "2000", :color => "blue", :engine_size => "4L", :number_of_doors => "4"})
       test_vehicle2.save()
       expect(Vehicle.find(test_vehicle.id())).to(eq(test_vehicle))
     end
@@ -81,21 +75,21 @@ describe('Vehicle') do
 
   describe("#color") do
     it "returns the color of the vehicle" do
-      test_vehicle = vehicle.new("Toyota", "Prius", 2000, "blue", "4l", "4")
+      test_vehicle = Vehicle.new({:make=> "Toyota", :model => "Prius", :year => "2000", :color => "blue", :engine_size => "4L", :number_of_doors => "4"})
       expect(test_vehicle.color).to(eq("blue"))
     end
   end
 
   describe("#engine_size") do
     it "returns the engine size for the vehicle" do
-      test_vehicle = vehicle.new("Toyota", "Prius", 2000, "blue", "4l", "4")
+      test_vehicle = Vehicle.new({:make=> "Toyota", :model => "Prius", :year => "2000", :color => "blue", :engine_size => "4l", :number_of_doors => "4"})
       expect(test_vehicle.engine_size).to(eq("4l"))
     end
   end
 
   describe("#number_of_doors") do
     it "returns the number of doors" do
-      test_vehicle = vehicle.new("Toyota", "Prius", 2000, "blue", "4l", "4")
+      test_vehicle = Vehicle.new({:make=> "Toyota", :model => "Prius", :year => "2000", :color => "blue", :engine_size => "4L", :number_of_doors => "4"})
       expect(test_vehicle.number_of_doors).to(eq("4"))
     end
   end
